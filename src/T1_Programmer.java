1d6e903576fe8423b0da691a3955a543dc66211f
Talon <1604690196@qq.com>
2022-05-15 15:31:03 +0800

    基本GUI完成

--- src/T1_Programmer.java
+++ src/T1_Programmer.java
@@ -1,2 +1,51 @@
+import javax.swing.*;
+
 public class T1_Programmer {
+    private JPanel root;
+
+    private JButton OFFButton;
+    private JButton BACKSPACEButton;
+    private JButton DiveideButton;
+    private JButton MultipButton;
+    private JButton MinusButton;
+    private JButton AddButton;
+    private JButton EqualButton;
+    private JButton a2SCButton;
+    private JButton DotButton;
+    private JButton a0Button;
+    private JButton DECButton;
+    private JButton CLCButton;
+    private JButton SHEButton;
+    private JButton ORButton;
+    private JButton ANDButton;
+    private JButton XORButton;
+    private JButton a1SCButton;
+    private JButton dButton;
+    private JButton eButton;
+    private JButton fButton;
+    private JButton aButton;
+    private JButton bButton;
+    private JButton cButton;
+    private JButton a9Button;
+    private JButton a6Button;
+    private JButton a3Button;
+    private JButton a8Button;
+    private JButton a7Button;
+    private JButton a4Button;
+    private JButton a5Button;
+    private JButton a2Button;
+    private JButton a1Button;
+    private JButton HEXButton;
+    private JButton LeftButton;
+    private JButton RightButton;
+
+    private JLabel ans;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("T1_Programmer");
+        frame.setContentPane(new T1_Programmer().root);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
