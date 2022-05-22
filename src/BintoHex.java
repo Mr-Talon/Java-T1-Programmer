4580787425c890185f5b98097f24b39ba80530ea
Lyr3 <lyric31415926@gmail.com>
2022-05-22 12:24:42 +0800

    完善十六进制和十进制负数转换

---
+++ src/BintoHex.java
@@ -0,0 +1,65 @@
+public class BintoHex {
+        private static String hexStr="0123456789ABCDEF";
+private static String[]binaryArray=
+        {"0000","0001","0010","0011",
+        "0100","0101","0110","0111",
+        "1000","1001","1010","1011",
+        "1100","1101","1110","1111"};
+public static String bytes2BinStr(byte[]bArray){
+
+        String outStr="";
+        int pos=0;
+        for(byte b:bArray){
+        //高四位
+        pos=(b&0xF0)>>4;
+        outStr+=binaryArray[pos];
+        //低四位
+        pos=b&0x0F;
+        outStr+=binaryArray[pos];
+        }
+        return outStr;
+        }
+
+/**
+ *
+ * @param bytes
+ * @return 将二进制数组转换为十六进制字符串  2-16
+ */
+public static String bin2HexStr(byte[]bytes){
+
+        String result="";
+        String hex="";
+        for(int i=0;i<bytes.length;i++){
+        //字节高4位
+        hex=String.valueOf(hexStr.charAt((bytes[i]&0xF0)>>4));
+        //字节低4位
+        hex+=String.valueOf(hexStr.charAt(bytes[i]&0x0F));
+        result+=hex;  //+" "
+        }
+        return result;
+        }
+
+/**
+ *
+ * @param hexString
+ * @return 将十六进制转换为二进制字节数组   16-2
+ */
+public static byte[]hexStr2BinArr(String hexString){
+        //hexString的长度对2取整，作为bytes的长度
+        int len=hexString.length()/2;
+        byte[]bytes=new byte[len];
+        byte high=0;//字节高四位
+        byte low=0;//字节低四位
+        for(int i=0;i<len;i++){
+        //右移四位得到高位
+        high=(byte)((hexStr.indexOf(hexString.charAt(2*i)))<<4);
+        low=(byte)hexStr.indexOf(hexString.charAt(2*i+1));
+        bytes[i]=(byte)(high|low);//高地位做或运算
+        }
+        return bytes;
+        }
+
+public static String hexStr2BinStr(String hexString){
+        return bytes2BinStr(hexStr2BinArr(hexString));
+        }
+}
--- src/Translation.java
+++ src/Translation.java
@@ -1,3 +1,5 @@
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
 import java.util.Scanner;
 import java.util.*;
 public class Translation {
@@ -5,29 +7,45 @@ public class Translation {
     public Translation(String s){
         this.s=s;
     };
-    public String Decimal() {
-
-        long num = Long.parseLong(s, 16);
+    public String Decimal() //十六进制转十进制
+    {
+        if(s.length()<8) //判断十六进制代表正负数
+        { long num = Long.parseLong(s, 16);//十六进制代表正数
         String ss = String.valueOf(num);
-        return ss;
+        return ss;}
+
+        else //十六进制代表负数或者正数
+        {
+            char firstCharacter = s.charAt(0);
+            if(firstCharacter>'7')//当十六进制首字符大于’7‘是负数
+            {
+                String ss=BintoHex.hexStr2BinStr(s);//调用BintoHEX方法获得二进制字符串
+                BigInteger bi = new BigInteger(ss, 2);
+                int final1=bi.intValue();
+                return String.valueOf(final1);
+
+            }
+            else{
+                long num = Long.parseLong(s, 16);//正数调用parseLong方法
+                String ss = String.valueOf(num);
+            }
+
+        }
+        return s;
     }
     public String Hexadecimal() {
-        int n=Integer.parseInt(s);
-        String ss="";
-        char c=' ';
-        if(n==0) {
-            ss="0";
+        BigInteger bi = new BigInteger(s);
+        byte[] byteInteger = bi.toByteArray();//调用BigInteger方法获取十进制对应二进制数组
+        String ss=BintoHex.bin2HexStr(byteInteger);
+        if(ss.length()<8)
+        {
+            StringBuilder sb=new StringBuilder();
+            for (int i = 0; i < 8-ss.length(); i++) {//补足十六进制F数量
+                sb.append("F");
+            }
+            return sb+ss;
         }
-        int m;
-        while(n!=0) {
-            m=n%16;
-            if(m/10==1)
-                c=(char)(65+(m%10));
-            else if(m/10==0)
-                c=(char)(48+m);
-            ss=c+ss;
-            n=n/16;
-        }
-        return ss;
+        return BintoHex.bin2HexStr(byteInteger);
     }
+
 }
