dd1f91805eff8067ad991fdffde2ad829a67bfb2
Lyr3 <lyric31415926@gmail.com>
2022-05-22 14:05:58 +0800

    十六进制二进制互转

--- src/Translation.java
+++ src/Translation.java
@@ -1,3 +1,7 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 import java.math.BigInteger;
 import java.nio.ByteBuffer;
 import java.util.Scanner;
@@ -12,8 +16,8 @@ public class Translation {
         if(s.length()<8) //判断十六进制代表正负数
         { long num = Long.parseLong(s, 16);//十六进制代表正数
         String ss = String.valueOf(num);
-        return ss;}
-
+        return ss;
+        }
         else //十六进制代表负数或者正数
         {
             char firstCharacter = s.charAt(0);
@@ -23,21 +27,21 @@ public class Translation {
                 BigInteger bi = new BigInteger(ss, 2);
                 int final1=bi.intValue();
                 return String.valueOf(final1);
-
             }
             else{
                 long num = Long.parseLong(s, 16);//正数调用parseLong方法
                 String ss = String.valueOf(num);
+                return s;
             }
 
         }
-        return s;
     }
     public String Hexadecimal() {
+        char array[]=s.toCharArray();
         BigInteger bi = new BigInteger(s);
         byte[] byteInteger = bi.toByteArray();//调用BigInteger方法获取十进制对应二进制数组
         String ss=BintoHex.bin2HexStr(byteInteger);
-        if(ss.length()<8)
+        if(ss.length()<8&&array[0]=='-')
         {
             StringBuilder sb=new StringBuilder();
             for (int i = 0; i < 8-ss.length(); i++) {//补足十六进制F数量
@@ -45,7 +49,16 @@ public class Translation {
             }
             return sb+ss;
         }
-        return BintoHex.bin2HexStr(byteInteger);
+        char array1[]=ss.toCharArray();
+        if(array1[0]=='0') {String strNew = ss.substring(1, ss.length());//删除转换后字符串首字符'o'
+        return strNew;}
+        return ss;
     }
+    /*public static void main(String[] args) {
+        Translation tr=new Translation("121231111");    //调试主函数
+        String s= tr.Hexadecimal();
+        System.out.println(s);
+
+    }*/
 
 }
