f27c0e565a3e2e256ced33a599e2c707c9367e81
Lyr3 <lyric31415926@gmail.com>
2022-05-26 00:24:37 +0800

    修復十进制转十六进制bug

--- src/Translation.java
+++ src/Translation.java
@@ -37,30 +37,39 @@ public class Translation {
         }
     }
     public String Hexadecimal() {
-        char array[] = s.toCharArray();
-        BigInteger bi = new BigInteger(s);
-        byte[] byteInteger = bi.toByteArray();//调用BigInteger方法获取十进制对应二进制数组
-        String ss = BintoHex.bin2HexStr(byteInteger);
-        if (ss.length() < 8 && array[0] == '-') {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < 8 - ss.length(); i++) {//补足十六进制F数量
-                sb.append("F");
-            }
-            return sb + ss;
+        if(Integer.parseInt(s)>=0&&Integer.parseInt(s)<=16){
+            int num=Integer.parseInt(s);
+            String str1=Integer.toHexString(num);
+            return str1.toUpperCase();
+
         }
-        char array1[] = ss.toCharArray();
-        if (array1[0] == '0') {
-            String strNew=ss.substring(1,ss.length());
-            while (array1[0] == '0') {
-                strNew = strNew.substring(1, strNew.length());
-                array1 = strNew.toCharArray();//删除转换后字符串首字符'o'}
+        else
+            {
+                char array[] = s.toCharArray();
+                BigInteger bi = new BigInteger(s);
+                byte[] byteInteger = bi.toByteArray();//调用BigInteger方法获取十进制对应二进制数组
+                String ss = BintoHex.bin2HexStr(byteInteger);
+                if (ss.length() < 8 && array[0] == '-') {
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < 8 - ss.length(); i++) {//补足十六进制F数量
+                        sb.append("F");
+                    }
+                    return sb + ss;
+                }
+                char array1[] = ss.toCharArray();
+                if (array1[0] == '0') {
+                    String strNew = ss.substring(1, ss.length());
+                    while (array1[0] == '0') {
+                        strNew = strNew.substring(1, strNew.length());
+                        array1 = strNew.toCharArray();//删除转换后字符串首字符'o'}
+                    }
+                    return strNew;
+                }
+                return ss;
             }
-            return strNew;
-        }
-        return ss;
     }
     /*public static void main(String[] args) {
-        Translation tr=new Translation("184");    //调试主函数
+        Translation tr=new Translation("14");    //调试主函数
         String s= tr.Hexadecimal();
         System.out.println(s);
 
